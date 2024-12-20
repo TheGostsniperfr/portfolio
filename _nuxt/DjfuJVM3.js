1d3ae24f96cccee95478c2f619b781295900b1e5
TheGostsniperfr <TheGostsniperfr@users.noreply.github.com>
2024-12-20 18:55:05 +0000

    deploy: 7110de07316239da65ca77a29edf4500ee41f554

---
+++ _nuxt/DjfuJVM3.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./DxMcDpxB.js";const s={},t={class:"test"};function a(_,n){return o(),c("p",t,"Hello world")}const d=e(s,[["render",a],["__scopeId","data-v-6b6aca87"]]);export{d as default};
