c5f858f4e48bf27ea4bc8f46d1b1d62d72691c7e
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2025-01-27 22:31:31 +0100

    ✅ Edit config for dns

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,6 @@
 import { defineNuxtConfig } from '@nuxt/bridge'
 
 export default defineNuxtConfig({
-  app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/portfolio/' : ''
-  },
-
-  runtimeConfig: {
-    public: {
-      baseURL: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
-    }
-  },
-
   ssr: false,
   compatibilityDate: '2024-11-13'
 })
